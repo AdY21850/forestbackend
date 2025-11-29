bd5a87a92dfe7986838a591325ad74caeb38f598
AdY21850 <135639134+AdY21850@users.noreply.github.com>
2025-11-29 08:31:35 +0530

    'add image and videos

--- forest-backend/src/main/java/com/forest/forest_backend/entity/Product.java
+++ forest-backend/src/main/java/com/forest/forest_backend/entity/Product.java
@@ -1,23 +1,34 @@
 package com.forest.forest_backend.entity;
+
 import jakarta.persistence.*;
 import lombok.Data;
 import java.math.BigDecimal;
-
+import java.util.List; // Import List so we can store multiple images
 
 @Data
 @Entity
-@Table (name="products")
+@Table(name = "products")
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     @Column(length = 1000)
     private String description;
+
     private BigDecimal price;
-    private String imageUrl;
+
+
+    @ElementCollection
+    @CollectionTable(name = "product_images", joinColumns = @JoinColumn(name = "product_id"))
+    @Column(name = "image_url")
+    private List<String> imageUrls;
+
+
+    private String videoUrl;
 
     @ManyToOne
     @JoinColumn(name = "category_id")
