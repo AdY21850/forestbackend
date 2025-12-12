fa675aae5b7983387f349e67651947565bf0e473
AdY21850 <135639134+AdY21850@users.noreply.github.com>
2025-12-13 01:25:44 +0530

    ipv6

--- forest-backend/src/main/java/com/forest/forest_backend/ForestBackendApplication.java
+++ forest-backend/src/main/java/com/forest/forest_backend/ForestBackendApplication.java
@@ -3,12 +3,13 @@ package com.forest.forest_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+
 @EnableAsync
 @SpringBootApplication
 public class ForestBackendApplication {
 
-	public static void main(String[] args) {
-
+    public static void main(String[] args) {
+        SpringApplication.run(ForestBackendApplication.class, args);
     }
 
 }
